1be7eea422eea25053b629c7e81de55510d16033
ramge <ram.gerassy@gmail.com>
2025-06-23 01:23:21 +0300

    added player movement on the screen

--- main.py
+++ main.py
@@ -15,8 +15,9 @@ def main():
                 return
         screen.fill((0,0,0))
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
-        dt = clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
